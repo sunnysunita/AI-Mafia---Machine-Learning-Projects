aeb2028c1ba3dbb0ef06d4293e4c5c0b601af227
sunnysunita <sunnysunita.com@gmail.com>
2019-10-19 13:18:13 -0700

    no message

--- numpy_assignment.py
+++ numpy_assignment.py
@@ -3,3 +3,10 @@ import numpy as np
 
 # 2. Print the numpy version and the configuration  (★☆☆)"
 print(np.__version__)
+
+# 3. Create a null vector of size 10 (★☆☆)"
+arr=np.zeros(10)
+print(arr)
+
+# 4.  How to find the memory size of any array (★☆☆)"
+print(len(arr))
