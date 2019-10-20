964b0f03fefe3bb47faadc87218f4d818fc3e202
sunnysunita <sunnysunita.com@gmail.com>
2019-10-20 02:20:03 -0700

    Create a custom dtype that describes a color as four unsigned bytes (RGBA)

--- numpy_assignment.py
+++ numpy_assignment.py
@@ -112,3 +112,9 @@ mat8=((mat7-np.mean(mat7))/np.std(mat7))
 #print(mat7)
 print(mat8)
 
+# 23. Create a custom dtype that describes a color as four unsigned bytes (RGBA) (★☆☆)"
+color = np.dtype([("r", np.ubyte, (1,)),
+                  ("g", np.ubyte, (1,)),
+                  ("b", np.ubyte, (1,)),
+                  ("a", np.ubyte, (1,))])
+print(color)
